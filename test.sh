8a295ad1fc0dbfb92b2639c7e46643056babc14b
mnotti <markusnotti@MacBook-Pro.local>
2015-04-09 22:53:38 -0700

    pseudoCommandTree Basic Functionality
    when passing a token buf into handle token buff, the tokens are made
    into commands and organized into a command tree in the stacks…lacking
    much implementation (like subtle commands, input/outputs, some other
    misc stuff), but overall it works, and the rest shouldn’t be hard to
    implement

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 a && b;
-b ^ c;
-a || b #This is a comment
-c || d;
-adsaknsdlkasndlknsdlknsdalkns
-a ls 
+b || c;
+a | b ;#This is a comment
+c||d;
+a && b;
+b || c;
+a | b ;#This is a comment
+c||d;
+
